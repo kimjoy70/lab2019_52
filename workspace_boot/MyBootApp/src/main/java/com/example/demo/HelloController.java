6a3098f7c1007a02ac899f22f33ad3d2935b427c
javassam <kimjoy70@naver.com>
2019-07-13 14:03:13 +0900

    스프링부트

---
+++ workspace_boot/MyBootApp/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/")
+	public String index() {
+		return "Hello Spring-Boot World!!";
+	}
+	@RequestMapping("/{num}")
+	public String test(@PathVariable int num) {
+		int res = 0;
+		for(int i=1;i<=num;i++) {
+			res+=i;
+		}
+		return "total: "+res;
+	}
+    @GetMapping("/{name}.html")
+    public String page(@PathVariable String name, Model model) {
+        model.addAttribute("pageName", name);
+        return "page";
+    }	
+}
---
+++ workspace_boot/MyBootApp/src/main/webapp/WEB-INF/jsp/dept/getDeptDetail.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>부서 정보 상세</title>
+</head>
+<body>
+<h2>부서 정보 상세보기</h2>
+</body>
+</html>
